009f1f1e77f23b8be084b63ad6c8fb9a070f5e4d
Blugil <dagnello@asu.edu>
2024-05-02 17:01:11 -0700

    basic command line flag parsing using the default go library

--- gtor.go
+++ gtor.go
@@ -1,32 +1,48 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gotorrent/p2p"
 	"gotorrent/torrentfile"
-	"os"
 )
 
 func main() {
 
-  args := os.Args[1:]
-  inPath := args[0]
-  outPath := args[1]
+  inPath := flag.String("t", "", "torrent file for download")
+  outPath := flag.String("o", ".", "the download output path")
+  resumePath := flag.String("r", "", "input partial download gtor file to resume")
 
-  tf, err := torrentfile.Open(inPath)
+
+  flag.Parse()
+
+  fmt.Println("in path is: ", *inPath)
+  fmt.Println("out path is: ", *outPath)
+  fmt.Println("resume path is: ", *resumePath)
+
+  tf, err := torrentfile.Open(*inPath)
   if err != nil {
     panic(err)
   }
 
   peers, err := tf.RequestPeers()
 
+
   t := p2p.Torrent {
     Peers: peers,
     PeerID: tf.PeerID,
     TF: tf,
   }
+
+  if *inPath == "" {
+    panic(fmt.Errorf("No input file passed in"))
+  }
+
+  resume := *resumePath != ""
   
-  err = t.DownloadTorrent(outPath, false)
+  fmt.Println("resume:", resume)
+
+  err = t.DownloadTorrent(*outPath, resume)
   if err != nil {
     fmt.Println(err)
   }
